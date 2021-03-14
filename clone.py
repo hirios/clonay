35cc32dec98c491a87d28031a935c9e1def869ad
Alan Rios <35049559+hirios@users.noreply.github.com>
2021-03-13 22:32:39 -0300

    tentativa de refatorar

--- clone.py
+++ clone.py
@@ -1,8 +1,9 @@
-from downIMGS import downIMGS
+from downIMGS import downIMGS, get_name, baixar
 from lxml import html
 import asyncio
 import aiohttp
 import aiofiles 
+import os
 
 
 headers = {
@@ -10,12 +11,55 @@ headers = {
     }
 
 
-def get_img_urls(html_string: str) -> list: 
-    """ Retorna uma lista de urls referentes as imagens de um conteúdo de resposta html (type string) """
+class HTML():
+    def __init__(self, html_string, url_to_clone):
+        self.tree = html.fromstring(html_string)
+        self.url_to_clone = url_to_clone
+        
 
-    tree = html.fromstring(html_string)
-    imgs = tree.xpath('//img/@src')
-    return imgs
+    def clean_html(self):
+        """ 
+        Remove todos os atributos srcset e data-srcset, necessário em alguns sites para carregar imagens via script """
+
+        for tag in self.tree.xpath('//*[@srcset]'):
+             tag.attrib.pop('srcset')
+
+        for tag in self.tree.xpath('//*[@data-srcset]'):
+             tag.attrib.pop('data-srcset')
+
+
+    def get_files(self) -> list: 
+        """ Retorna uma lista de urls referentes as imagens de um conteúdo de resposta html (type string) """
+
+        # CAPTURANDO URLS DE IMAGENS, CSS, JSON, JS... RETORNA LISTAS
+
+        imgs = self.tree.xpath('//img/@src')
+        imgs2 = self.tree.xpath('//img/@data-src')
+        scripts = self.tree.xpath('//script/@src')
+        css_and_json = self.tree.xpath('//link/@href')
+
+
+        # ALGUMAS URL NÃO TÊM O CAMINHO COMPLETO ATÉ O ARQUIVO;
+        # NESTES CASOS CONCATENO O FRAGMENTO DA URL COM A URL DO SITE 
+        # EX: /exemplo/teste/imagem.png
+        # SITE: http://www.google.com/
+
+        for x in range(0, len(css_and_json)):
+            if css_and_json[x].startswith('/'):
+                split = self.url_to_clone.split('/')[:3]
+
+                # ESSE TIPO DE ARQUIVO EU BAIXO ANTES POR COMPLICAÇÕES POSTERIORES AO ASSOCIAR OS ARQUIVOS OFLINE
+                asyncio.run(baixar(split[0] + '//' + split[2] + css_and_json[x]))
+    
+        imgs = imgs + imgs2 + scripts + css_and_json
+        return imgs
+
+
+    def run(self):
+        self.clean_html()
+        imgs = self.get_files()
+        self.tree = html.tostring(self.tree).decode('UTF-8')
+        return imgs, self.tree
 
 
 count = 0
@@ -23,18 +67,24 @@ async def clone(URL):
     """ Salva a html de um site num arquivo index.html """ 
 
     global count
-    
-    async with aiohttp.ClientSession() as session:
+
+    timeout = aiohttp.ClientTimeout(total=5)
+    async with aiohttp.ClientSession(timeout=timeout, headers=headers) as session:
         async with session.get(URL) as page:
             count += 1
             if page.status == 200:
                 file = await aiofiles.open(f'index{str(count)}.html', 'w', encoding='UTF-8')
                 content = await page.text() 
 
-                urls = get_img_urls(content)
-                downImgs(urls)
+                urls, content = HTML(content, URL).run()        
+                downIMGS(urls)
+                
+                for x in urls:
+                    content = content.replace('src="' + x + '"', 'src="' + os.getcwd() + '\\imagens\\' + get_name(x) + '"')
+                    content = content.replace('href="' + x + '"', 'href="' + os.getcwd() + '\\imagens\\' + get_name(x) + '"')
+                    content = content.replace('src=' + x, 'src="' + os.getcwd() + '\\imagens\\' + get_name(x) + '"')
 
-                await file.write(content.replace('src="https://', 'src="'))
+                await file.write(content)
             else:
                 print('[-] STATUS_CODE ERRO:', page.status)
 
@@ -44,9 +94,9 @@ async def main():
 
 
 # EXEMPLO
-URL1 = 'https://www.pinterest.pt/nikitalitos/melhores-fotos-de-animes-geral/'
-URL2 = 'https://madeinjapan.com.br/2017/05/23/top-100-melhores-animes-da-historia/'
-coroutines_list = [clone(URL1), clone(URL)]
+URL1 = 'https://realpython.com/'
+URL2 = 'https://www.tecmundo.com.br/cultura-geek/146716-anime-8-melhores-episodios-piloto-series.htm'
+coroutines_list = [clone(URL1), clone(URL2)]
 
 
 if __name__ == "__main__":
--- downIMGS.py
+++ downIMGS.py
@@ -5,36 +5,65 @@ import time
 import nest_asyncio
 
 
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
+    }
+
+
 # permitindo asyncio.run() a partir de outro módulo
 nest_asyncio.apply()
 
 
-async def baixar(url):
+def get_name(url: str):
+    """ Essa função tenta retornar o nome dos arquivos a partir da url de download """
+
+    # O NOME DO ARQUIVO GERALMENTE VEM NO FINAL DA URL (PATH)
     name = url.split('/')[-1]
+    TAGS = ('.jpg', '.jpeg', '.png', '.gif', '.css', '.json', '.js', '.ico')
 
-    if '.jpg' in name:
-        name = name.split('.jpg?')[0] + '.jpg'
-        
-    elif '.jpeg' in name:
-        name = name.split('.jpeg?')[0] + '.jpeg'
-
-    elif '.png' in name:
-        name = name.split('.png?')[0] + '.png'
-
-
-    if url.startswith('http'):
-        try:
-            async with aiohttp.ClientSession() as session:
-                async with session.get(url) as resp:
-                    if resp.status == 200:
-                        f = await aiofiles.open(f'imagens/{name}', mode='wb')
-                        await f.write(await resp.read())
-                        await f.close()                                
-        except:
-            print('[-] Erro ao baixar imagem; url:', url)
-            pass
-
+    # AQUI VERIFICAMOS SE O NOME QUE PEGAMOS TERMINA COM UMAS DESSAS EXTENSÕES
+    if name.endswith(TAGS):
+        return name
+    
+    # CASO NÃO TERMINE COM NENHUMA DESSAS EXTENSÕES, ELE TENTA PEGAR E DIVIDIR UM TRECHO 
+    # PARECIDO COM ISSO: teste.com/imagem.png?exemplo/outra_coisa
+    for x in TAGS:
+        if not x.endswith('?'):
+            x = x + '?'
             
+            if x in name:
+                name = name.split(x)[0] + x[:-1]
+            else:
+                if x[1:] in name:
+                    name = name.split(x[1:])[0] +  x[1:-1]
+
+    return name
+
+
+async def baixar(url: str):
+    """ Realiza o download de algum arquivo """
+    
+    name = get_name(url)
+
+    if name:
+        if url.startswith('http'):
+            try:
+                timeout = aiohttp.ClientTimeout(total=10)
+                async with aiohttp.ClientSession(headers=headers, timeout=timeout) as session:
+                    async with session.get(url) as resp:
+                        if resp.status == 200:
+                            f = await aiofiles.open(f'imagens/{name}', mode='wb')
+                            await f.write(await resp.read())
+                            await f.close()                                
+            except:
+                print('[-] Erro ao baixar arquivo; url:', url)
+                #print('[-] Nome usado para salvar:', name)
+                pass
+
+    else:
+        print('[-] Nome para salvar vazio:', str(len(name)))
+
+
 def createMain(lista):
     complemento = ""
     for x in lista:
@@ -48,7 +77,7 @@ async def main2():
     return funcao 
 
 
-def downImgs(lista):
+def downIMGS(lista):
     s = time.perf_counter()
     main = createMain(lista)
     exec(main, globals())
@@ -56,3 +85,11 @@ def downImgs(lista):
     elapsed = time.perf_counter() - s
     print(f"executed in {elapsed:0.2f} seconds.")
 
+
+if __name__ == "__main__":
+    url_list = ['urls']
+
+    if lista[0] == 'urls':
+        print('Passe suas urls para a "url_list"')
+        quit()
+    asyncio.run(downIMGS(lista))
